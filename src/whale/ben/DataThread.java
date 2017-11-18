87ae094ffcca2fbed57325051b3fde6a50a331e0
Ben Whale <benwhale@hotmail.co.uk>
2017-11-18 16:57:28 +0000

    aasfsdgdsadfsa

---
+++ src/whale/ben/DataThread.java
@@ -0,0 +1,48 @@
+package whale.ben;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class DataThread extends Thread {
+	private DataInput data;
+	private boolean hasNewData = false;
+	private String[] line;
+	private LinkedBlockingQueue<Double> q;
+	
+	public DataThread(LinkedBlockingQueue<Double> q) {
+		
+		
+		data = new DataInput();
+		this.q = q;
+	}
+
+	public void run() {
+		
+		while(true) {
+		line = data.getNextMinute();
+		try {
+			q.put(Double.parseDouble(line[4]));
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			sleep(6000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	}
+	
+	/**
+	 * Gives the % change +/- since last polled
+	 */
+	public double getChange() {
+		
+		return 0.0;
+	}
+	
+}
---
+++ src/whale/ben/MusicThread.java
@@ -0,0 +1,25 @@
+package whale.ben;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class MusicThread extends Thread {
+	private LinkedBlockingQueue<Double> q;
+	
+	public MusicThread(LinkedBlockingQueue<Double> q) {
+		this.q = q;
+		
+		
+	}
+
+	public void setChange(double change) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void run() {
+		
+		
+		
+	}
+
+}
