7f2ed1b9b7c53c3866d0513b8140ccc958fd268d
Sam Whale <32313989+SamWhale@users.noreply.github.com>
2017-11-18 20:16:53 +0000

    it works

--- src/whale/sound/NextSong.java
+++ src/whale/sound/NextSong.java
@@ -1,32 +1,35 @@
-import java.net.*;
+import com.mashape.unirest.http.Unirest;
+import java.lang.*;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Base64.Encoder;
+
 
 public class NextSong {
-  static final String OAuth = "BQCah9nJ7Zxvq1UpaCggT0jn5Jzf1EVGOeWzOuTsFfbdgdtsqvKIQtdr7CamCwyEuj7ZpMDGw772Exm3KVMTAlDNQwXArsej75dRTHsdd2qKW4X4Lip7TJYDCUPMnAnq-HHTeEb0rUdiSasR";
-  static final String ID = "1f60082d34864c39b752ace10f5ff00d";
-  static final String Secret = "b933f2341509494da1a6e878e0338e5e";
+    static final String OAuth = "BQCah9nJ7Zxvq1UpaCggT0jn5Jzf1EVGOeWzOuTsFfbdgdtsqvKIQtdr7CamCwyEuj7ZpMDGw772Exm3KVMTAlDNQwXArsej75dRTHsdd2qKW4X4Lip7TJYDCUPMnAnq-HHTeEb0rUdiSasR";
+    static final String ID = "1f60082d34864c39b752ace10f5ff00d";
+    static final String Secret = "b933f2341509494da1a6e878e0338e5e";
 
-  public static String httpGET(String inputURL) throws Exception{
-    HttpURLConnection connection = null;
-    URL getURL = new URL(inputURL);
-    connection = (HttpURLConnection) getURL.openConnection();
-    BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+    public static void main(String[] args) throws Exception{
 
-    String inputLine;
-    String outputLine = "";
 
-    while ((inputLine = in.readLine()) != null) { //get each line
-      outputLine = outputLine + inputLine + "\n";
-      }
+        String literalDumpster = "https://requestb.in/qznm79qz";
+        String myURL = "https://accounts.spotify.com/api/token";
 
-    in.close(); //closes connection
-    return outputLine.substring(0,outputLine.length()-1); //snips off the extra newline at the end
+        byte[] encodedBytes = (ID+":"+Secret).getBytes(StandardCharsets.UTF_8);
+        Encoder encode = Base64.getEncoder();
+        String encodedString = encode.encodeToString(encodedBytes);
+
+
+        String result = Unirest.post(myURL)
+                .header("Authorization", "Basic " + encodedString)
+                .header("Accept", "application/json")
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .body("grant_type=client_credentials")
+                .asString()
+                .getBody();
+
+        System.out.println(result);
     }
-
-  public static void main(String[] args) throws Exception{
-    String OAuth = "BQCah9nJ7Zxvq1UpaCggT0jn5Jzf1EVGOeWzOuTsFfbdgdtsqvKIQtdr7CamCwyEuj7ZpMDGw772Exm3KVMTAlDNQwXArsej75dRTHsdd2qKW4X4Lip7TJYDCUPMnAnq-HHTeEb0rUdiSasR";
-    System.out.println(httpGET("https://accounts.spotify.com/authorize?client_id=1f60082d34864c39b752ace10f5ff00d&response_type=code&redirect_uri=http://localhost:8888/callback"));
-    //System.out.println(httpGET("\"https://api.spotify.com/v1/recommendations?min_energy=0.4&market=US&seed_tracks=0c6xIDDpzE81m2q797ordA&seed_artists=4NHQUGzhtTLFvgF5SZesLK&min_popularity=50&Accept=application/json&Authorization=BQCah9nJ7Zxvq1UpaCggT0jn5Jzf1EVGOeWzOuTsFfbdgdtsqvKIQtdr7CamCwyEuj7ZpMDGw772Exm3KVMTAlDNQwXArsej75dRTHsdd2qKW4X4Lip7TJYDCUPMnAnq-HHTeEb0rUdiSasR"));
-    }
-
-  }
+}
